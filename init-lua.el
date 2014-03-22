b9e15d97d5ee6cbe97cc749d913b38d096b36205
Zach Massia <zmassia@gmail.com>
2014-03-22 16:18:14 -0400

    Use autopair with Lua

--- init-lua.el
+++ init-lua.el
@@ -1,11 +1,14 @@
 (require 'init-utils)
 
 (install-pkg-list '(lua-mode))
+(require 'lua-mode)
 
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
 
 (setq lua-indent-level 2)
+
+(add-hook 'lua-mode-hook 'autopair-mode)
 
 (provide 'init-lua)
