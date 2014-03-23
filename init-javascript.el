524ccfa6c197c81020bb493387a6ae3f9c9c3a18
Zach Massia <zmassia@gmail.com>
2014-03-23 12:58:05 -0400

    Actually use js2-mode

--- init-javascript.el
+++ init-javascript.el
@@ -3,4 +3,13 @@
 (install-pkg-list '(js2-mode
                     js2-refactor))
 
+(require 'js2-mode)
+
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (autopair-on)))
+
+
 (provide 'init-javascript)
