852e79def8122dd703737251433b7ef35a944a40
Zachary Massia <zmassia@gmail.com>
2014-05-10 11:10:10 -0400

    Set tab-width to 4

--- init-python.el
+++ init-python.el
@@ -3,7 +3,10 @@
 (install-pkg-list '(jedi
                     py-autopep8))
 
-(add-hook 'python-mode-hook 'jedi:setup)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (jedi:setup)
+            (setq tab-width 4)))
 (setq jedi:complete-on-dot t)
 
 (provide 'init-python)
