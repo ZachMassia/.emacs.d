4a1f09ce4a1368fd422e1a87ac33469a17a6557f
Zachary Massia <zmassia@gmail.com>
2015-03-29 16:47:05 -0400

    Add fn rename-file-and-buffer

--- lisp/zach-utils.el
+++ lisp/zach-utils.el
@@ -98,4 +98,17 @@ A `spec' can be a `read-kbd-macro'-readable string or a vector."
     (cofi/set-key map (car mapping) (cadr mapping)))
   map)
 
+(defun rename-file-and-buffer ()
+  "Rename the current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
+
 (provide 'zach-utils)
