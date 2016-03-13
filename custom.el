675c1a5b43f1000a50ca6dc4e09a67dbbf55cd05
Zach Massia <zmassia@gmail.com>
2016-03-12 20:47:35 -0500

    Remove custom-set-faces
    Removing the mostly empty custom-set-faces call in custom.el
    fixed the problem of the set-frame-font call in the init file
    to not persist on Linux.

--- custom.el
+++ custom.el
@@ -47,9 +47,3 @@
      (compilation-scroll-output t)
      (projectile-project-compilation-cmd . "make PLATFORMIO_BUILD")
      (projectile-project-run-cmd . "make PLATFORMIO_UPLOAD")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background nil)))))
